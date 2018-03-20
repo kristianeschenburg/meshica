2cd6fd8185ecf8640f6edfd1663b769df988afa5
Kristian Eschenburg <keschenb@uw.edu>
2018-03-20 13:36:44 -0700

    dual regression skeleton

---
+++ CanICA/dual_regression.py
@@ -0,0 +1,23 @@
+class DualRegression(object):
+
+    def __init(self):
+
+        """
+
+        :return:
+        """
+
+        pass
+
+    def fit(self,input_files,group_components):
+
+        """
+
+        :param input_files:
+        :param group_components:
+        :return:
+        """
+
+        pass
+
+    def _
