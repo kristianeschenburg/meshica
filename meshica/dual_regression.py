fa08e1a28e1dc3c6956b436a44e45eab6e841663
Kristian Eschenburg <keschenb@uw.edu>
2018-09-10 13:01:11 -0700

    prints

--- meshica/dual_regression.py
+++ meshica/dual_regression.py
@@ -58,7 +58,7 @@ class DualRegression(object):
 
         for inp in input_files:
 
-            print 'Loading {:}'.format(inp.split('/')[-1])
+            print('Loading {:}'.format(inp.split('/')[-1]))
 
             matrix = load.load(inp)
             signals.append(matrix)
@@ -75,7 +75,7 @@ class DualRegression(object):
         :return:
         """
 
-        print 'Temporal regression.'
+        print('Temporal regression.')
 
         models = {}.fromkeys(np.arange(len(signals)))
         time_series = []
@@ -98,7 +98,7 @@ class DualRegression(object):
         :return: Z-scores of spatial maps
         """
 
-        print 'Spatial regression.'
+        print('Spatial regression.')
 
         models = {}.fromkeys(np.arange(len(signals)))
         S = StandardScaler(with_mean=False, with_std=True)
